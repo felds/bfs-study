029f9131bab523fc97866dbcf7bc8470c9905e8c
Felds Liscia <dev@felds.com.br>
2024-05-17 23:11:18 -0300

    memoize results

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
-// console.log("Hello via Bun!");
-
 type Pair = [number, number]
 
+const getMultiplierPairsCache: Map<number, Pair[]> = new Map()
 export function getMultiplierPairs(n: number) {
+    if (getMultiplierPairsCache.has(n)) {
+        return getMultiplierPairsCache.get(n)!
+    }
+
     const start = Math.trunc(Math.sqrt(n))
-    const pairs = []
+    const pairs: Pair[] = []
     for (let a = start; a > 1; a--) {
         const b = n / a
         if (Number.isInteger(b)) {
             pairs.push([a, b])
         }
     }
+
+    getMultiplierPairsCache.set(n, pairs)
     return pairs
 }
