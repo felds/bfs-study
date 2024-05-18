95c58b7b5c85c55b997824168feb455b454a80f1
Felds Liscia <dev@felds.com.br>
2024-05-17 23:31:42 -0300

    test more squares

--- index.test.ts
+++ index.test.ts
@@ -11,8 +11,11 @@ test("It generates all the multipliers", () => {
     expect(getMultiplierPairs(5)).toEqual([])
     expect(getMultiplierPairs(19)).toEqual([])
 
-    // composite numbers
+    // squares
     expect(getMultiplierPairs(4)).toEqual([[2, 2]])
+    expect(getMultiplierPairs(25)).toEqual([[5, 5]])
+
+    // composite numbers
     expect(getMultiplierPairs(6)).toEqual([[2, 3]])
     expect(getMultiplierPairs(12)).toEqual([
         [3, 4],
